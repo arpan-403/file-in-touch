4f12001e6943c5fd886154cabfa667318bfba6a3
arpan-403 <161766306+arpan-403@users.noreply.github.com>
2024-04-25 16:55:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const express=require("express");
 const app=express();
-var cors = require('cors')
-
-app.use(cors())
+const cors = require('cors');
+const corsOptions ={
+    origin:'https://file-in-touch.vercel.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 const PORT=process.env.PORT || 5000
 const login=require("./Routes/login");
 const isAuthenticate=require("./Routes/isAuthenticate");
