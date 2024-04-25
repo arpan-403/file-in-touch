cc32c3f72c5b8c02bc141ca493896ca242843eae
arpan-403 <161766306+arpan-403@users.noreply.github.com>
2024-04-25 18:52:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
 require('dotenv').config()
 const express=require("express");
 const app=express();
-const cors = require('cors');
-const corsOptions = {
-  origin: 'https://file-in-touch.vercel.app',
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-};
+// const cors = require('cors');
+// const corsOptions = {
+//   origin: 'https://file-in-touch.vercel.app',
+//   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+// };
 
-// Add CORS middleware with specific options
-app.use(cors(corsOptions));
+// // Add CORS middleware with specific options
+// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", 'https://file-in-touch.vercel.app');
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
 const PORT=process.env.PORT || 5000
 const login=require("./Routes/login");
 const isAuthenticate=require("./Routes/isAuthenticate");
