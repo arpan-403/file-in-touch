c5b116c500b32ca6514916c04bea00d9b9d05942
arpan-403 <161766306+arpan-403@users.noreply.github.com>
2024-04-25 18:43:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,12 @@ const app=express();
 // };
 
 // app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 const PORT=process.env.PORT || 5000
 const login=require("./Routes/login");
 const isAuthenticate=require("./Routes/isAuthenticate");
