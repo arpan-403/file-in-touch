0b4a712f68b786cb39932d34ce3964169bdf5b76
arpan-403 <161766306+arpan-403@users.noreply.github.com>
2024-04-25 15:53:25 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -19,7 +19,7 @@ function Login({setLogin,setUserId,setRegisterModel}) {
           console.log("in login");
           let data;
         try {
-          data=await axios.post("/api/login",{email,password},config);
+          data=await axios.post("https://file-in-touch-api.vercel.app/api/login",{email,password},config);
         } catch (error) {
           setLogin(false);
           setRegisterModel(true);
