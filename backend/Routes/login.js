0ff29cdad75e2333f003bce641d02821121e12e2
arpan-403 <161766306+arpan-403@users.noreply.github.com>
2024-04-25 16:14:58 +0530

    Update login.js

--- backend/Routes/login.js
+++ backend/Routes/login.js
@@ -34,7 +34,7 @@ router.post("/api/login",async(req,res)=>{
             await res.cookie("jwt",token,{
                 maxAge:24*60*60*1000
             });
-            res.send({id:document._id});
+            res.send({id:document._id,token});
         }
     }
     
